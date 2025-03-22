531ce213aa6398d5e072bfea5aab4152543f2206
D-Zoro <nishu142k@gmail.com>
2025-03-23 02:03:57 +0530

    major frontend part done -home page logo plus idhar udhar ka kam

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
-//import './App.css'
-
-import{BrowserRouter as Router,Route, Routes} from "react-router-dom";
+import React from "react";
+import{BrowserRouter as Router,Routes,Route,Navigate,useParams} from "react-router-dom";
+//components
 import Login from './components/login/login';
+import Home from './components/home/Home';
+import Navbar from "./components/navbar/navbar";
+import { Children } from "react";
+
+  //  authorized router uncomment later;
+// const PrivateRoute=({ children }) => {
+//   const{ username: usernameFromUrl }=useParams(); //get username from url
+//   const token=localStorage.getItem('token');
+//   const usernameFromStorage=localStorage.getItem('tokenUser');
+
+//   if(!token || usernameFromUrl !==usernameFromStorage){
+//     localStorage.removeItem('token');
+//     localStorage.removeItem('tokenUser');
+//     return <Navigate to ="/unauthorizedAccess"/>
+//   }
+//   return children;
+// };
 
 
 
@@ -12,7 +26,11 @@ function App() {
   return(
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login"element={<Login/>}/>
+        {/* sign up */}
+        {/* aboutus */}
+        {/* etc etc */}
       </Routes>
     </Router>
    
