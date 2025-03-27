92d670aad505916bc8f43b533a018fd2ca53f56c
Nasrat <nasratulla2006@gmail.com>
2025-03-27 17:06:26 +0530

    Added an Aboutus page and renamed the navbar file to Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import{BrowserRouter as Router,Routes,Route,Navigate,useParams} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useParams,
+} from "react-router-dom";
 //components
-import Login from './components/login/login';
-import Home from './components/home/Home';
-import Navbar from "./components/navbar/navbar";
+import Login from "./components/login/login";
+import Home from "./components/home/Home";
+import Navbar from "./components/navbar/Navbar";
 import { Children } from "react";
+import AboutUs from "./components/Aboutus/Aboutus";
 
-  //  authorized router uncomment later;
+//  authorized router uncomment later;
 // const PrivateRoute=({ children }) => {
 //   const{ username: usernameFromUrl }=useParams(); //get username from url
 //   const token=localStorage.getItem('token');
@@ -20,22 +27,19 @@ import { Children } from "react";
 //   return children;
 // };
 
-
-
 function App() {
-  return(
+  return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login"element={<Login/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/aboutus" element={<AboutUs />} />
         {/* sign up */}
         {/* aboutus */}
         {/* etc etc */}
       </Routes>
     </Router>
-   
   );
-  
 }
 
 export default App;
